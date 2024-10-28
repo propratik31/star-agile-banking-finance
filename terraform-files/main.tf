11178f664fe61213ab45da6d797d0d424ee90c55
Pratik Anil Yadav <91407633+propratik31@users.noreply.github.com>
2024-10-28 19:57:27 +0530

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "test-server" {
   ami = "ami-06b21ccaeff8cd686"
   instance_type = "t2.micro"
-  key_name = "mykey"
-  vpc_security_group_ids = ["sg-04cf4e3801bd09203"]
+  key_name = "myjenkins"
+  vpc_security_group_ids = ["sg-05b5859d9fb7b7c98"]
   connection {
      type = "ssh"
      user = "ec2-user"
-     private_key = file("./mykey.pem")
+     private_key = file("./myjenkins.pem")
      host = self.public_ip
      }
   provisioner "remote-exec" {
